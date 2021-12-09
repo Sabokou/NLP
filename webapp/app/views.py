8d7e9ac92473dab37bfd5948b0bdc3c1aaa8339d
alina <alina.buss@yahoo.de>
2021-12-09 17:37:20 +0100

    Basic WebApp

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -2,6 +2,15 @@ from flask import render_template
 from app import app
 
 
-@app.route('/')  # Home
+@app.route('/')  # Overview
 def index():
     return render_template("/index.html")
+
+@app.route('/learning', methods=['POST', 'GET'])  # Learning
+def learning():
+    return render_template("/learning.html")
+
+@app.route('/exercise', methods=['POST', 'GET'])  # Exercise
+def exercise():
+    return render_template("/exercise.html")
+
