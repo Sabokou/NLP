e8e4c1e9c29a4359fff03d5a2a09bb42bbb5fe3c
alina <alina.buss@yahoo.de>
2021-12-16 11:26:50 +0100

    Upload-Functionality

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -6,4 +6,5 @@ app = Flask(__name__, instance_relative_config=True)
 # Load the config file
 app.config.from_object('config')
 
+
 from app import views  # Load the views
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,7 +1,21 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
 from app import app
+from werkzeug.utils import secure_filename
+import os
+
+uploads_dir = os.path.join(app.root_path, 'Uploads')
+os.makedirs(uploads_dir, exist_ok=True)
+app.config['SECRET_KEY'] = 'mysecretkey'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'docx'}
+
+# General Functions
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+
+# Page Functions
 @app.route('/')  # Overview
 def index():
     return render_template("/index.html")
@@ -13,4 +27,29 @@ def learning():
 @app.route('/exercise', methods=['POST', 'GET'])  # Exercise
 def exercise():
     return render_template("/exercise.html")
+
+@app.route('/upload', methods=['POST', 'GET'])  # Upload
+def upload():
+    return render_template("/upload.html")
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploading_file():
+   if request.method == 'POST':
+       if 'file' not in request.files:
+           flash('No file part')
+           return render_template("/includes/fail.html", title='Error',
+                           text='No file part')
+       file = request.files['file']
+       # If the user does not select a file, the browser submits an
+       # empty file without a filename.
+       if file.filename == '':
+           flash('No selected file')
+           return render_template("/includes/fail.html", title='Error',
+                           text='No file was selected')
+       if file and allowed_file(file.filename):
+           filename = secure_filename(file.filename)
+           path=os.path.join(uploads_dir, filename)
+           file.save(path)
+           return render_template("/includes/success.html", title='Success',
+                                   text='Your file has been uploaded successfully.')
 
